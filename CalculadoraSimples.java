3320d92bfa6dd43d33ad0e3292377b596c4291ed
Samuel260107 <130668952+Samuel260107@users.noreply.github.com>
2023-05-04 09:23:25 -0300

    commit

---
+++ CalculadoraSimples.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class CalculadoraSimples1{
+
+    public static void main(String[] args) {
+
+        //Declaração de variáveis
+        int n1, n2, total, op;
+
+        //Criação e instância do objeto de entrada
+        Scanner entrada = new Scanner(System.in);
+
+        do{
+
+            System.out.println("\n\t\t\t -- Calculadora Simples --\n");
+            System.out.println("1. Soma");
+            System.out.println("2. Subtração");
+            System.out.println("3. Sair");
+
+            System.out.print(" Opção: ");
+            op = entrada.nextInt();
+               
+            if(op == 1){
+                System.out.println("\n\t\t\t -- Soma --\n");
+                //Entrada
+                System.out.println("Informe N1: ");
+                n1 = entrada.nextInt();
+                System.out.println("Informe N2: ");
+                n2 = entrada.nextInt();
+
+                //Processamento
+                total = n1 + n2;
+
+                //Saida
+                System.out.printf("%d + %d = %d", n1, n2, total);
+            }else if (op == 2){
+                
+                //Entrada
+                System.out.println("Informe N1: ");
+                n1 = entrada.nextInt();
+                System.out.println("Informe N2: ");
+                n2 = entrada.nextInt();
+
+                //Processamento
+                total = n1 - n2;
+
+                //Saida
+                System.out.printf("%d + %d = %d", n1, n2, total);
+
+                System.out.println("\n\t\t\t -- Subtração --\n");
+            }else if(op == 3){
+                System.out.println("Forte abraço!");
+            }else{
+                System.out.println("Opção" + op + " incorreta!");
+            }
+
+        }while(op!=3);
+        
+
+    }
+
+    
+}
